ae1c2bb1840c330729f682515111f3ed76594bb4
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-04-29 15:10:41 +0900

    feat : 다른 방식으로 json파일을 읽어오도록 수정
    - 기존의 기능이 배포했을때 문제가 되어 수정함

--- be/sidedish/src/main/java/com/codesquad/sidedish/util/SecretUtil.java
+++ be/sidedish/src/main/java/com/codesquad/sidedish/util/SecretUtil.java
@@ -2,10 +2,7 @@ package com.codesquad.sidedish.util;
 
 import com.codesquad.sidedish.init.dto.ServerSecretDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.util.ResourceUtils;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -17,8 +14,7 @@ public class SecretUtil {
 
     public static void initServerSecretDto() {
         try {
-            File serverSecretFile = ResourceUtils.getFile("classpath:server.secret.json");
-            InputStream inputStream = new FileInputStream(serverSecretFile);
+            InputStream inputStream = SecretUtil.class.getResourceAsStream("/server.secret.json");
             ObjectMapper objectMapper = new ObjectMapper();
             SERVER_SECRET_DTO = objectMapper.readValue(inputStream, ServerSecretDTO.class);
         } catch (IOException e) {
