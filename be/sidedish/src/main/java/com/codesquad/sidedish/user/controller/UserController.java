b85f95d31246d8e62d9569758a077a4b0c21ec22
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-04-29 16:41:07 +0900

    feat : 네이티브 앱으로 토큰을 전달할 수 있도록 수정
    - 해당 기능을 위해서 웹페이지를 동적으로 만들어서 보내야 했음
    - 그래서 핸들바를 사용하기로 함
    - 추가로 UserController를 일반 Controller로 수정함

--- be/sidedish/src/main/java/com/codesquad/sidedish/user/controller/UserController.java
+++ be/sidedish/src/main/java/com/codesquad/sidedish/user/controller/UserController.java
@@ -1,22 +1,22 @@
 package com.codesquad.sidedish.user.controller;
 
 import com.codesquad.sidedish.user.dto.ReceiveAccessTokenDTO;
-import com.codesquad.sidedish.user.dto.ResponseJwtTokenDTO;
 import com.codesquad.sidedish.user.dto.UserInfoDTO;
 import com.codesquad.sidedish.user.service.GoogleApiRequester;
 import com.codesquad.sidedish.user.service.UserService;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 import static com.codesquad.sidedish.util.UrlConstant.LOGIN_PAGE_URL;
 
-@RestController
+@Controller
 @RequestMapping("/users")
 public class UserController {
 
@@ -34,11 +34,13 @@ public class UserController {
     }
 
     @GetMapping("/callback")
-    public ResponseJwtTokenDTO oauthCallBack(@RequestParam("code") String code, @RequestParam("scope") String scope,
-                                             @RequestParam("authuser") String authuser, @RequestParam("prompt") String prompt) throws JsonProcessingException {
+    public String oauthCallBack(@RequestParam("code") String code, @RequestParam("scope") String scope,
+                                @RequestParam("authuser") String authuser, @RequestParam("prompt") String prompt,
+                                Model model) throws JsonProcessingException {
         ReceiveAccessTokenDTO receiveAccessTokenDTO = googleApiRequester.requestAccessToken(code);
         UserInfoDTO userInfoDTO = googleApiRequester.requestUserInfo(receiveAccessTokenDTO.getAccess_token());
         String jwtToken = userService.login(userInfoDTO);
-        return new ResponseJwtTokenDTO(jwtToken);
+        model.addAttribute("token", jwtToken);
+        return "token";
     }
 }
