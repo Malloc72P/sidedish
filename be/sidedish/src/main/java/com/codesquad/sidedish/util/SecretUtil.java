750c5baa7ee71954879e6b15036b1e449ad4e663
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-04-30 11:45:06 +0900

    feat : secret파일 읽는데 실패했을때 프로그램을 종료하도록 구현

--- be/sidedish/src/main/java/com/codesquad/sidedish/util/SecretUtil.java
+++ be/sidedish/src/main/java/com/codesquad/sidedish/util/SecretUtil.java
@@ -2,11 +2,15 @@ package com.codesquad.sidedish.util;
 
 import com.codesquad.sidedish.init.dto.ServerSecretDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 public class SecretUtil {
+    private static final Logger logger = LoggerFactory.getLogger(SecretUtil.class);
+    private static final String SECRET_FILE_IO_ERROR = "server.secret.json파일을 읽어오는 과정에서 문제가 발생했습니다!";
     private static ServerSecretDTO SERVER_SECRET_DTO;
 
     private SecretUtil() {
@@ -17,9 +21,15 @@ public class SecretUtil {
             InputStream inputStream = SecretUtil.class.getResourceAsStream("/server.secret.json");
             ObjectMapper objectMapper = new ObjectMapper();
             SERVER_SECRET_DTO = objectMapper.readValue(inputStream, ServerSecretDTO.class);
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException | IllegalArgumentException e) {
+            onSecretFileIoException(e);
         }
+    }
+
+    private static void onSecretFileIoException(Exception e) {
+        logger.error(SECRET_FILE_IO_ERROR);
+        e.printStackTrace();
+        System.exit(1);
     }
 
     public static String clientId() {
